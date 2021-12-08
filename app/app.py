6c9b37af7d3d89b51f2d10d48a9258fd592adeac
danielratmiroff <danielratmiroff@gmail.com>
2021-12-08 12:16:38 +0100

    prgss

--- app/app.py
+++ app/app.py
@@ -6,62 +6,35 @@ from pymongo import MongoClient, results
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
-load_dotenv()
+# load_dotenv()
 
-DB_USER = os.getenv('MONGO_USER')
-DB_PASSWORD = os.getenv('MONGO_PASSWORD')
-DB_IP = os.getenv('MONGO_IP')
-DB_PORT = os.getenv('MONGO_PORT')
+# DB_USER = os.getenv('MONGO_USER')
+# DB_PASSWORD = os.getenv('MONGO_PASSWORD')
+# DB_IP = os.getenv('MONGO_IP')
+# DB_PORT = os.getenv('MONGO_PORT')
 
-url = f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_IP}:{DB_PORT}'
-client = MongoClient(url)
-db = client["nft-ums"]
-col = db["user"]
-
-
-# Todo
-# create a local mongodb container
-# add environmental variables for testing and deployment
-# create a replica mongodb set for
-
-user = col.find({})
-
-print(list(user))
-
-# UserNum.insert_one({
-#     "users": 0
-# })
+# url = f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_IP}:{DB_PORT}'
+# client = MongoClient(url)
+# db = client["nft-ums"]
+# col = db["user"]
 
 
-# class Get(Resource):
-#     def get(self):
-#         return "hey world!"
+# # Todo
+# # create a local mongodb container
+# # add environmental variables for testing and deployment
+# # create a replica mongodb set for
+
+# user = col.find({})
+
+# print(list(user))
 
 
-# class Visit(Resource):
-#     def get(self):
-#         prev_num: UserNum.find({})[0]["users"]
-#         new_num = prev_num + 1
-#         UserNum.insert({}, {"$set": {"users": new_num}})
-#         return str("Hello user " + str(new_num))
+@app.route('/', methods=['GET'])
+def add():
+    return 'Hello World'
 
 
-# class Add(Resource):
-#     def post(self):
-#         postedData = request.get_json()
-#         dataE = {
-#             'Message': postedData,
-#             'Status Code': 200
-#         }
-#         return jsonify(dataE)
-
-
-# api.add_resource(Add, '/add')
-# api.add_resource(Visit, '/hello')
-# api.add_resource(Get, '/')
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
