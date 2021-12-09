c38deeca3e88737f35c92614974179616f52e33e
danielratmiroff <danielratmiroff@gmail.com>
2021-12-09 17:03:40 +0100

    fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/ums
-docker-compose build --no-cache
-MONGO_USER=$MONGO_USER MONGO_PASSWORD=$MONGO_PASSWORD MONGO_IP=$MONGO_IP MONGO_PORT=$MONGO_PORT docker-compose up -d
+docker-compose rm --all &&
+docker-compose build --no-cache &&
+docker-compose --env-file ~/.env up up -d --force-recreate
