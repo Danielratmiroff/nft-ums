494f9d6c4b6b6e6d4441e92cf46ff3ec719d62c9
danielratmiroff <danielratmiroff@gmail.com>
2021-12-09 16:22:56 +0100

    mongo variables

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/ums
 docker-compose build --no-cache
-docker-compose up -d
+MONGO_USER=$MONGO_USER MONGO_PASSWORD=$MONGO_PASSWORD MONGO_IP=$MONGO_IP MONGO_PORT=$MONGO_PORT docker-compose up -d
