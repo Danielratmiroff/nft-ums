60f651b12eb356a5dd3b09578f16dde9b9fbbc2a
danielratmiroff <danielratmiroff@gmail.com>
2021-12-11 11:22:05 +0100

    Progress

--- app/app.py
+++ app/app.py
@@ -1,20 +1,21 @@
 import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+from flask_cors import CORS, cross_origin
 from pymongo import MongoClient, results
-
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app, support_credentials=True)
 
+api = Api(app)
 load_dotenv()
 
-DB_USER = os.getenv('MONGO_USER')
-DB_PASSWORD = os.getenv('MONGO_PASSWORD')
-DB_IP = os.getenv('MONGO_IP')
+DB_USER = os.getenv("MONGO_USER")
+DB_PASSWORD = os.getenv("MONGO_PASSWORD")
+DB_IP = os.getenv("MONGO_IP")
 
-url = f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_IP}:27017'
+url = f"mongodb://{DB_USER}:{DB_PASSWORD}@{DB_IP}:27017"
 client = MongoClient(url)
 db = client["nft-ums"]
 col = db["user"]
@@ -23,15 +24,16 @@ col = db["user"]
 # Todo
 # create a replica mongodb set for production
 
+
 class Visit(Resource):
     def get(self):
         user = col.find({})
         value = list(user)
-        return f'Hello World: {value}'
+        return f"Hello World: {value}"
 
 
-api.add_resource(Visit, '/')
+api.add_resource(Visit, "/")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
