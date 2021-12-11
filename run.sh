83b98b2387d8ab325f36afce0cfb6253e942bce6
danielratmiroff <danielratmiroff@gmail.com>
2021-12-11 13:09:54 +0100

    Progress

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/ums
-docker-compose rm &&
+docker-compose down && docker-compose rm &&
     docker-compose build --no-cache &&
-    docker-compose --env-file ~/.env up -d --force-recreate
+    docker-compose up -d --force-recreate
