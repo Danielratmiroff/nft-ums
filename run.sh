8932c88f2973f884ba65597373832f0bb79cc487
danielratmiroff <danielratmiroff@gmail.com>
2021-12-11 12:27:55 +0100

    Progress

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/ums
+echo ~/.env
+echo $MONGO_USER
 docker-compose rm &&
     docker-compose build --no-cache &&
     docker-compose --env-file ~/.env up -d --force-recreate
